22c35a33bc47185984eed1077948ac9136bb44b7
choyj109 <choyj109@gmail.com>
2022-06-28 23:50:23 +0900

    경로 수정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
 import Main from "./components/Main";
-import Move from "./components/Move";
-import Guide from "./components/Guide";
-import Diagnosis from "./components/Diagnosis";
-import Learning from "./components/Learning";
-import Story from "./components/Story";
-import Mento from "./components/Mento";
-import Post from "./components/Post";
-import Mypage from "./components/Mypage";
-import Login from "./components/Login";
 
 function App() {
   return (
@@ -18,16 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/main" element={<Main />}></Route>
-          <Route path="/main/move" element={<Move />}></Route>
-          <Route path="/main/guide" element={<Guide />}></Route>
-          <Route path="/main/diagnosis" element={<Diagnosis />}></Route>
-          <Route path="/main/learning" element={<Learning />}></Route>
-          <Route path="/main/story" element={<Story />}></Route>
-          <Route path="/main/mento" element={<Mento />}></Route>
-          <Route path="/main/post" element={<Post />}></Route>
-          <Route path="/main/mypage" element={<Mypage />}></Route>
-          <Route path="/main/login" element={<Login />}></Route>
+          <Route path="/main/*" element={<Main />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import Header from "./Header";
+import { Routes, Route } from "react-router-dom";
+import Move from "./Move";
+import Guide from "./Guide";
+import Diagnosis from "./Diagnosis";
+import Learning from "./Learning";
+import Story from "./Story";
+import Mento from "./Mento";
+import Post from "./Post";
+import Mypage from "./Mypage";
+import Login from "./Login";
 
 const Main = () => {
   const title = "expert";
   return (
     <div className="main">
       <Header title={title} />
+      <Routes>
+        <Route path="/main/move" element={<Move />}></Route>
+        <Route path="/main/guide" element={<Guide />}></Route>
+        <Route path="/main/diagnosis" element={<Diagnosis />}></Route>
+        <Route path="/main/learning" element={<Learning />}></Route>
+        <Route path="/main/story" element={<Story />}></Route>
+        <Route path="/main/mento" element={<Mento />}></Route>
+        <Route path="/main/post" element={<Post />}></Route>
+        <Route path="/main/mypage" element={<Mypage />}></Route>
+        <Route path="/main/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 };
