75141de4ddf48d9e412de8eade8f72c78dc20d19
jhyun-k <jhkim1238@naver.com>
2022-07-06 14:30:17 +0900

    input

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
+
 const Login = () => {
   return (
     <div className="login">
-      <h1>로그인페이지</h1>
-      <h1>push</h1>
+      <container>
+      <input id="id" name="id" placeholder="아이디를 입력해주세요" />
+      <input
+        id="password"
+        name="password"
+        type="password"
+        placeholder="비밀번호를 입력해주세요"
+      />
+      <button>로그인</button>
+    </container>
     </div>
   );
 };
