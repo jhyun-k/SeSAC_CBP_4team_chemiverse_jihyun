7c015d1b10761129a97e0b2b6443779daebbb3f5
choyj109 <choyj109@gmail.com>
2022-07-06 09:12:28 +0900

    learningDetail

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,17 @@ const Nav = () => {
     { content: "마이페이지", href: "./mypage" },
     { content: "로그인", href: "./login" },
   ];
-
+  const guideDetail = [
+    { content: "공지사항", href: "./guide" },
+    { content: "시간표", href: "./guide/timetable" },
+    { content: "강사소개", href: "./guide/teacher" },
+  ];
+  const learningDetail = [
+    { content: "회사이해", href: "./learning" },
+    { content: "워크스킬", href: "./learning/workskill" },
+    { content: "피플스킬", href: "./learning/peopleskill" },
+    { content: "자기관리", href: "./learning/selfmanage" },
+  ];
   return (
     <nav className="nav">
       <ul>
@@ -25,6 +35,38 @@ const Nav = () => {
           );
         })}
       </ul>
+      <div className="tabmenu">
+        <ul>
+          {guideDetail.map((element, index) => {
+            return (
+              <li key={index}>
+                <NavLink to={element.href}>{element.content}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          {learningDetail.map((element, index) => {
+            return (
+              <li key={index}>
+                <NavLink to={element.href}>{element.content}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+        <ul>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+        </ul>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </nav>
   );
 };
