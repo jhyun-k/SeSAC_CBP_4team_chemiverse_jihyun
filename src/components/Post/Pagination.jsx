18203320793adbee9bd03f6469074a800918790c
Jiyeong Jeong <8533283@naver.com>
2022-07-08 12:14:17 +0900

    modify pagination

--- src/components/Post/Pagination.jsx
+++ src/components/Post/Pagination.jsx
@@ -1,19 +1,71 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Pagination = ({ total, page, setPage, LIST_PER_PAGE }) => {
   const pagesNum = Math.ceil(total / LIST_PER_PAGE);
-  const pagesNumArray = new Array(pagesNum);
+  const wholePagesNumArray = new Array(pagesNum);
+
+  const [pagesArrStartNum, setPagesArrStartNum] = useState(0);
+  const [pagesArrEndNum, setPagesArrEndNum] = useState(5);
+
+  const handlePrevBtnClick = () => {
+    setPage(page - 5);
+    setPagesArrStartNum(pagesArrStartNum - 5);
+    setPagesArrEndNum(pagesArrEndNum - 5);
+  };
+  const handleNextBtnClick = () => {
+    setPage(page + 5);
+    setPagesArrStartNum(pagesArrStartNum + 5);
+    setPagesArrEndNum(pagesArrEndNum + 5);
+  };
+
+  const handleFirstBtnClick = () => {
+    setPage(1);
+    setPagesArrStartNum(0);
+    setPagesArrEndNum(5);
+  };
+  const handleLastBtnClick = () => {
+    setPage(wholePagesNumArray.length);
+    setPagesArrStartNum(wholePagesNumArray.length - 5);
+    setPagesArrEndNum(wholePagesNumArray.length);
+    console.log(pagesArrStartNum, pagesArrEndNum);
+  };
+
+  const pagesNumArray = wholePagesNumArray.fill().map((arr, index) => {
+    return index + 1;
+  });
+
+  console.log(pagesArrStartNum, pagesArrEndNum, pagesNum);
+
   return (
     <div>
-      <button>이전</button>
-      {pagesNumArray.fill().map((element, index) => {
-        return (
-          <button key={index + 1} onClick={() => setPage(index + 1)}>
-            {index + 1}
-          </button>
-        );
-      })}
-      <button>이후</button>
+      <button onClick={handleFirstBtnClick} disabled={pagesArrStartNum === 0}>
+        처음
+      </button>
+      <button onClick={handlePrevBtnClick} disabled={pagesArrStartNum == 0}>
+        이전
+      </button>
+      {pagesNumArray
+        .slice(pagesArrStartNum, pagesArrEndNum)
+        .map((element, index) => {
+          return (
+            <button key={index} onClick={() => setPage(element)}>
+              {element}
+            </button>
+          );
+        })}
+      <button
+        onClick={handleNextBtnClick}
+        disabled={pagesArrEndNum >= pagesNum}
+      >
+        이후
+      </button>
+      <button
+        onClick={handleLastBtnClick}
+        disabled={pagesArrEndNum >= pagesNum}
+      >
+        끝
+      </button>
     </div>
   );
 };
