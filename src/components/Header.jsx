b9191dc64f855433c10649d77f26f1a710625cbe
jhyun-k <jhkim1238@naver.com>
2022-07-14 15:56:11 +0900

    login modal finreal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import Nav from "./Nav";
 import { Link, NavLink } from "react-router-dom";
-import "./style/header.module.css";
+import headerStyle from "./style/header.module.css";
 import { useNavigate } from 'react-router-dom';
+import { FiSearch } from "react-icons/fi";
 
 
 
@@ -15,31 +16,33 @@ const Header = ({ title,auth,setAuth,setIsOpen}) => {
     function openModal() {
       setIsOpen(true);
     }
-
-    
-  
    
   return (
-    <header className="main-header-box">
-      <div className="main-header">
-      
-        <h1>
-          <Link to="/">{title}</Link>
-        </h1>
-        <Nav />
-        <div>
-          <ul>
-          <NavLink to='./mypage'>마이페이지</NavLink>
+    <header className={headerStyle.mainHeaderBox}>
+      <div className={headerStyle.inner}>
+        <ul className={headerStyle.login}>
+        <NavLink to='./mypage'>마이페이지</NavLink>
           <div className='login_btn'>
              {
                 auth === true ? 
                 <span onClick={()=>setAuth(false)}>
-                    로그아웃
+                    로그아웃  
                 </span> : 
                 <span onClick={()=>{goToLogin();openModal()}}>로그인</span>
              }
             </div>
-          </ul>
+        </ul>
+        <div className={headerStyle.mainHeader}>
+          <h1>
+            <Link to="/">{title}</Link>
+          </h1>
+          <Nav />
+          <form className={headerStyle.searchBox}>
+            <input type="text" placeholder="검색어를 입력하세요" />
+            <button>
+              <FiSearch />
+            </button>
+          </form>
         </div>
       </div>
     </header>
