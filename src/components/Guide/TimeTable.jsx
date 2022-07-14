1025cd96789675c15812d29bc9902e8f716f1d8b
choyj109 <choyj109@gmail.com>
2022-07-14 19:55:58 +0900

    calendar

--- src/components/Guide/TimeTable.jsx
+++ src/components/Guide/TimeTable.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Calendar from "./CalendarDay";
 
 const TimeTable = () => {
   return (
     <div className="timetable">
       <h1>시간표</h1>
+      <Calendar />
     </div>
   );
 };
