cddcd85daa852a4a9ec74cb16aa4b81d110d0d05
brighten <brighhten@gmail.com>
2022-07-14 15:40:52 +0900

    merge

--- src/components/Post/PostDetail.jsx
+++ src/components/Post/PostDetail.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import styles from './css/PostDetail.module.css';
 
-const PostDetail = () => {
-  return <div></div>;
+const PostDetail = ({ data }) => {
+  const { post } = useParams();
+  const selectedPost = data.find((x) => {
+    return x.id == post;
+  });
+
+  console.log(selectedPost);
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.headerBox}>
+        <h2 className={styles.title}>{selectedPost.title}</h2>
+        <div className={styles.postInfo}>
+          <span>{selectedPost.userId}</span>
+          <span>조회수</span>
+          <span>2022.10.12</span>
+        </div>
+      </div>
+      <div className={styles.body}>{selectedPost.body}</div>
+      <div>
+        <Link to='/main/post'>
+          <button className={styles.listBtn}>목록</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default PostDetail;
