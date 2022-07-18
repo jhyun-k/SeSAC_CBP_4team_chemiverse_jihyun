ee729bb666dcebb07052c9b08c0b2adfda9134f9
jhyun-k <jhkim1238@naver.com>
2022-07-18 15:03:49 +0900

    homenav

--- src/components/Home/HomeHeaderNavy.jsx
+++ src/components/Home/HomeHeaderNavy.jsx
@@ -27,7 +27,7 @@ const HomeHeaderNavy = () => {
               })}
             </ul> 
             <button className={styles.onup_btn}>
-              <Link to="/main/login" >Chemiverse On & Up</Link>
+              <Link to="/login" >Chemiverse On & Up</Link>
             </button>
         </div>
     </header>
