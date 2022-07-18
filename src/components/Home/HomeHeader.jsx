5264aa94aba09aac7f07fae12997be248b9e35dd
choyj109 <choyj109@gmail.com>
2022-07-18 14:43:36 +0900

    종합 merge

--- src/components/Home/HomeHeader.jsx
+++ src/components/Home/HomeHeader.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD:src/components/Home/HomeHeader.jsx
-import styles from './css/HomeHeader.module.css' 
+import styles from "../Home/css/HomeHeader.module.css";
 
 const HomeHeader = () => {
-  
-
-
-
-=======
-import styles from "./HomeHeader.module.css";
-
-const HomeHeader = () => {
->>>>>>> 876124946690df082520b39933f57769df4c84c0:src/components/HomeHeader.jsx
   const homeList = [
     { content: "환영인사", href: "welcome" },
     { content: "회사소개", href: "introduce" },
@@ -21,27 +11,9 @@ const HomeHeader = () => {
   ];
   return (
     <header className={styles.homeHeader}>
-<<<<<<< HEAD:src/components/Home/HomeHeader.jsx
-        <div className={styles.homeContainer}>
-              <Link to="/" className={styles.blacklogo}>expert</Link>
-            <div className={styles.title}>Chemiverse Connect With</div>
-            {/* <ul className={styles.homeNav}>
-              {homeList.map((element, index) => {
-                return (
-                  <li className={styles.navlist} key={index}>
-                    <Link to={element.href}>{element.content}</Link>
-                  </li>
-                );
-              })}
-            </ul> */}
-            <button className={styles.onup_btn}>
-              <Link to="/login" >Chemiverse On & Up</Link>
-            </button>
-=======
       <div className={styles.homeContainer}>
         <div className={styles.blacklogo}>
           <Link to="/"></Link>
->>>>>>> 876124946690df082520b39933f57769df4c84c0:src/components/HomeHeader.jsx
         </div>
         <div className={styles.title}>Chemiverse Connect With</div>
         <ul className="homeNav">
