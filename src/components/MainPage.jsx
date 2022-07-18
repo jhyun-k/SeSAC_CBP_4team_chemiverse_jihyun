7e198d804b98409d59159d5630ec1c881270ebef
seonhwan <se0o0ver@gmail.com>
2022-07-18 14:43:52 +0900

    commit before merge

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Modal from "react-modal";
 import Loginok from "./Login/Loginok";
 import { useState } from "react";
-<<<<<<< HEAD
+import mainPageStyle from "./style/mainPage.module.css";
 
 const MainPage = () => {
     const mainList = [
@@ -17,7 +17,7 @@ const MainPage = () => {
         { content: "게시판", href: "./post" },
     ];
     return (
-        <div className="mainPage">
+        <div className={mainPageStyle.mainPage}>
             <ul>
                 {mainList.map((element, index) => {
                     return (
@@ -29,34 +29,6 @@ const MainPage = () => {
             </ul>
         </div>
     );
-=======
-import mainPageStyle from "./style/mainPage.module.css";
-
-const MainPage = () => {
-  const mainList = [
-    { content: "Connect With 바로가기", href: "/*" },
-    { content: "교육장 이동", href: "./move" },
-    { content: "교육안내", href: "./guide" },
-    { content: "진단", href: "./diagnosis" },
-    { content: "역량학습", href: "./learning" },
-    { content: "ChemiStory", href: "./chemiStory" },
-    { content: "멘토링", href: "./mentoring" },
-    { content: "게시판", href: "./post" },
-  ];
-  return (
-    <div className={mainPageStyle.mainPage}>
-      <ul>
-        {mainList.map((element, index) => {
-          return (
-            <li key={index}>
-              <Link to={element.href}>{element.content}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
->>>>>>> develop
 };
 
 export default MainPage;
