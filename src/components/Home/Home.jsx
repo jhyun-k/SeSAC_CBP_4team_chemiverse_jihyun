3273216a6cab820eb511dbb5e9f49e2259a417ad
seonhwan <se0o0ver@gmail.com>
2022-07-18 14:38:12 +0900

    merge

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,5 @@
 import React from "react";
 import HomeHeader from "./HomeHeader";
-<<<<<<< HEAD:src/components/Home/Home.jsx
-import { Routes,Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import Welcome from './Welcome';
-import Introduce from './Introduce';
-import Ready from './Ready';
-import HomeNotice from './HomeNotice';
-
-const Home = () => {
-      
-      
-  return (
-    <div className="home">
-      <HomeHeader />
-      <Link to='welcome'>환영인사</Link>
-      <Link to='introduce'>회사소개</Link>
-      <Link to='ready'>입사 전 준비사항</Link>
-      <Link to='homenotice'>게시판</Link> 
-=======
 import Welcome from "./Welcome";
 import Introduce from "./Introduce";
 import Ready from "./Ready";
@@ -26,23 +7,22 @@ import HomeNotice from "./HomeNotice";
 import { Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom";
 const Home = () => {
-  return (
-    <div className="home">
-      <HomeHeader />
-      <Link to="welcome">환영인사</Link>
-      <Link to="introduce">회사소개</Link>
-      <Link to="ready">입사 전 준비사항</Link>
-      <Link to="homenotice">게시판</Link>
->>>>>>> 876124946690df082520b39933f57769df4c84c0:src/components/Home.jsx
+    return (
+        <div className="home">
+            <HomeHeader />
+            <Link to="welcome">환영인사</Link>
+            <Link to="introduce">회사소개</Link>
+            <Link to="ready">입사 전 준비사항</Link>
+            <Link to="homenotice">게시판</Link>
 
-      <Routes>
-        <Route path="/welcome" element={<Welcome />}></Route>
-        <Route path="/introduce" element={<Introduce />}></Route>
-        <Route path="/ready" element={<Ready />}></Route>
-        <Route path="/homenotice" element={<HomeNotice />}></Route>
-      </Routes>
-    </div>
-  );
+            <Routes>
+                <Route path="/welcome" element={<Welcome />}></Route>
+                <Route path="/introduce" element={<Introduce />}></Route>
+                <Route path="/ready" element={<Ready />}></Route>
+                <Route path="/homenotice" element={<HomeNotice />}></Route>
+            </Routes>
+        </div>
+    );
 };
 
 export default Home;
