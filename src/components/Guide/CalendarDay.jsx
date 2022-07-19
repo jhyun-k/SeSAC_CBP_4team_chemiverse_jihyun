7203e8b1d12cc02ef55706fa056de0a1c655f6ee
choyj109 <choyj109@gmail.com>
2022-07-19 19:48:42 +0900

    달력 한글

--- src/components/Guide/CalendarDay.jsx
+++ src/components/Guide/CalendarDay.jsx
@@ -4,16 +4,35 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import calendar from "./css/calendar.module.css";
 import "./css/main.css";
 import { useState } from "react";
+import { formatDate } from "@fullcalendar/react";
 
 const CalendarDay = () => {
   const [title, setTitle] = useState([]);
   const [start, setStart] = useState([]);
+  const [end, setEnd] = useState([]);
+
   const eventClick = (info) => {
+    const format = formatDate(info.event.start, {
+      month: "long",
+      year: "numeric",
+      day: "numeric",
+      timeZone: "UTC",
+      locale: "ko",
+    });
+    const formatEnd = formatDate(info.event.end, {
+      month: "long",
+      year: "numeric",
+      day: "numeric",
+      timeZone: "UTC",
+      locale: "ko",
+    });
     info.jsEvent.preventDefault();
-    console.log(info.event.start);
     setTitle(info.event.title);
-    setStart(info.event.start);
-    console.log(start.start);
+    setStart(format);
+    setEnd(formatEnd);
+
+    console.log(start);
+    console.log(format);
   };
   return (
     <div className={calendar.mainBox}>
@@ -23,12 +42,13 @@ const CalendarDay = () => {
           {
             title: "00 신입사원 과정",
             start: "2022-07-19",
-            // end: "2022-07-21",
+            end: "2022-07-21",
             url: "http://google.com/",
           },
           {
             title: "Another Event",
             start: "2022-07-20",
+            end: "2022-07-22",
             url: "http://google.com/",
           },
         ]}
@@ -39,7 +59,8 @@ const CalendarDay = () => {
       />
       <div className={calendar.eventBox}>
         <h1 className={calendar.h1Event}>{title}</h1>
-        {/* <h2 className={calendar.h2Event}>{start}</h2> */}
+        <h2 className={calendar.h2Event}>{start}</h2>
+        <h2 className={calendar.h2Event}>{end}</h2>
       </div>
     </div>
   );
