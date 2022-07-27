9460254d2260c7bde63d078b20f9c80404c8db9a
choyj109 <choyj109@gmail.com>
2022-07-27 15:53:32 +0900

    calendar

--- src/components/Guide/CalendarDay.jsx
+++ src/components/Guide/CalendarDay.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
 import calendar from "./css/calendar.module.css";
 import "./css/main.css";
 import { useState } from "react";
 import { formatDate } from "@fullcalendar/react";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const CalendarDay = () => {
-  const [title, setTitle] = useState([]);
-  const [start, setStart] = useState([]);
-  const [end, setEnd] = useState([]);
-
-  const eventClick = (info) => {
+  /*   const eventClick = (info) => {
     const format = formatDate(info.event.start, {
       month: "long",
       year: "numeric",
@@ -27,10 +25,11 @@ const CalendarDay = () => {
       locale: "ko",
     });
     info.jsEvent.preventDefault();
-    setTitle(info.event.title);
+     setTitle(info.event.title);
     setStart(format);
-    setEnd(formatEnd);
-  };
+    setEnd(formatEnd); 
+  }; */
+
   return (
     <div className={calendar.mainBox}>
       <FullCalendar
@@ -38,29 +37,33 @@ const CalendarDay = () => {
         events={[
           {
             title: "00 신입사원 과정",
-            start: "2022-07-19",
-            end: "2022-07-21",
-            url: "http://google.com/",
+            start: "2022-07-21",
+            end: "2022-07-25",
           },
           {
             title: "Another Event",
             start: "2022-07-20",
             // end: "2022-07-22",
-            url: "http://google.com/",
           },
         ]}
-        defaultView="dayGridMonth"
-        plugins={[dayGridPlugin]}
-        eventClick={eventClick}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,listMonth",
+        }}
+        plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
         locale="ko"
+        selectable={true}
+        navLinks={true}
       />
-      <div className={calendar.eventBox}>
+      {/*  <div className={calendar.eventBox}>
         <h1 className={calendar.h1Event}>{title}</h1>
         <div className={calendar.h2Box}>
           <h2 className={calendar.h2Event}>{start}</h2>
           <h2 className={calendar.h2Event}>{end ? "~" + end : end}</h2>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
