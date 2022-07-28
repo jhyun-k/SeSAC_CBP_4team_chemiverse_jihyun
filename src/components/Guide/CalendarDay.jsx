a7a0be0ff75429476073ce6b48aa21724c0ea3cb
choyj109 <choyj109@gmail.com>
2022-07-28 15:26:47 +0900

    customizing

--- src/components/Guide/CalendarDay.jsx
+++ src/components/Guide/CalendarDay.jsx
@@ -6,30 +6,56 @@ import "./css/main.css";
 import { useState } from "react";
 import { formatDate } from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
-import Modal from "react-modal";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
 
 const CalendarDay = () => {
-  const [title, setTitle] = useState([]);
+  const handleEventClick = (clickInfo) => {
+    if (window.confirm(`${clickInfo.event.title} 일정을 삭제하시겠습니까? `)) {
+      clickInfo.event.remove();
+    }
+  };
+  const handleDateSelect = (selectInfo) => {
+    let title = prompt("제목을 작성해주세요");
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        display: "auto",
+        extendedProps: {
+          description: `${title} ${
+            selectInfo.start.getMonth() + 1
+          }.${selectInfo.start.getDate()}~${selectInfo.end.getMonth() + 1}.${
+            selectInfo.end.getDate() - 1
+          }`,
+        },
+      });
+    }
+  };
+
+  /* 
+   const [title, setTitle] = useState([]);
   const [start, setStart] = useState([]);
   const [end, setEnd] = useState([]);
-  const [modalIsOpen, setModalIsOpen] = useState(false);
-
-  const handleEventClick = (clickInfo) => {
-    const formatStart = formatDate(clickInfo.event.start, {
+  const formatStart = formatDate(info.event.start, {
       month: "long",
       year: "numeric",
       day: "numeric",
       timeZone: "UTC",
       locale: "ko",
     });
-    const formatEnd = formatDate(clickInfo.event.end, {
+    const formatEnd = formatDate(info.event.end, {
       month: "long",
       year: "numeric",
       day: "numeric",
       timeZone: "UTC",
       locale: "ko",
     });
-    setTitle(clickInfo.event.title);
+    setTitle(info.event.title);
     setStart(formatStart);
     setEnd(formatEnd);
     return (
@@ -42,59 +68,54 @@ const CalendarDay = () => {
           </h2>
         </div>
       </div>
-    );
-    /*  if (window.confirm(`정말로 삭제하시겠습니까? ${clickInfo.event.title}`)) {
-      clickInfo.event.remove();
-    } */
+    ); */
+
+  const handleTooltip = (info) => {
+    tippy(info.el, {
+      content: info.event.extendedProps.description,
+      placement: "top",
+    });
   };
-  const handleMouseEnter = (hoverInfo) => {};
-  const handleDateSelect = (selectInfo) => {
-    let title = prompt("제목을 작성해주세요");
-    let calendarApi = selectInfo.view.calendar;
-    calendarApi.unselect();
-    if (title) {
-      calendarApi.addEvent({
-        title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-      });
-    }
-  };
+
   return (
     <div className={calendar.mainBox}>
       <FullCalendar
         className={calendar.FullCalendar}
         events={[
           {
-            title: "00 신입사원 과정",
+            title: "신입사원 과정",
             start: "2022-07-19",
             end: "2022-07-21",
+            extendedProps: {
+              description: "신입사원 과정 7.19~7.20",
+            },
           },
           {
-            title: "Another Event",
-            start: "2022-07-20",
+            title: "교육안내",
+            start: "2022-07-22",
             // end: "2022-07-22",
+            extendedProps: {
+              description: "교육안내 7.22~7.22",
+            },
           },
         ]}
         initialView="dayGridMonth"
         headerToolbar={{
-          left: "prev,next",
+          left: "today",
           center: "title",
-          right: "today,dayGridMonth",
+          right: "prev,next",
         }}
         plugins={[dayGridPlugin, interactionPlugin]}
         locale="ko"
         selectable={true}
         select={handleDateSelect}
         eventClick={handleEventClick}
-        eventMouseEnter={handleMouseEnter}
         height="45vw"
+        eventDidMount={handleTooltip}
+        eventBackgroundColor="#9498D1"
+        eventBorderColor="#9498D1"
       />
-      <button onClick={() => setModalIsOpen(true)}>Modal Open</button>
-      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        This is Modal content
-        <button onClick={() => setModalIsOpen(false)}>Modal Open</button>
-      </Modal>
+
       {/* <div className={calendar.eventBox}>
         <h1 className={calendar.h1Event}>{title}</h1>
         <div className={calendar.h2Box}>
