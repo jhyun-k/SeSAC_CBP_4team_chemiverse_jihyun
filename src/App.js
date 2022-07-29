640ab68b09f0a121c1c59ca9d1b822fb6bbb8bd3
Jiyeong Jeong <8533283@naver.com>
2022-07-29 16:54:08 +0900

    try deploy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./components/Home/Home";
-import Main from "./components/Main";
-import { IoIosArrowDropupCircle } from "react-icons/io";
-import { useState, useEffect } from "react";
-import Welcome from "./components/Home/Welcome";
-import Ready from "./components/Home/Ready";
-import Introduce from "./components/Home/Introduce";
-import Loginok from "./components/Login/Loginok";
-import FindID from "./components/Login/FindID";
-import DetailRedirect from "./components/Login/DetailRedirect";
-import Board from "./components/Board/Board";
-import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home/Home';
+import Main from './components/Main';
+import { IoIosArrowDropupCircle } from 'react-icons/io';
+import { useState, useEffect } from 'react';
+import Welcome from './components/Home/Welcome';
+import Ready from './components/Home/Ready';
+import Introduce from './components/Home/Introduce';
+import Loginok from './components/Login/Loginok';
+import FindID from './components/Login/FindID';
+import DetailRedirect from './components/Login/DetailRedirect';
+import Board from './components/Board/Board';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -35,7 +35,7 @@ function App() {
     // 클릭하면 스크롤이 위로 올라가는 함수
     window.scrollTo({
       top: 0,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
     setScrollY(0); // ScrollY 의 값을 초기화
     setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
@@ -43,38 +43,38 @@ function App() {
 
   useEffect(() => {
     const watch = () => {
-      window.addEventListener("scroll", handleFollow);
+      window.addEventListener('scroll', handleFollow);
     };
     watch();
     return () => {
-      window.removeEventListener("scroll", handleFollow);
+      window.removeEventListener('scroll', handleFollow);
     };
   });
   useEffect(() => {}, [auth]);
 
   return (
-    <div className="App">
-      <BrowserRouter /* basename={process.env.PUBLIC_URL} */>
+    <div className='App'>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
-          <Route path="main/*" element={<DetailRedirect auth={auth} />} />
+          <Route path='main/*' element={<DetailRedirect auth={auth} />} />
           <Route
-            path="/main/*"
+            path='/main/*'
             element={<Main auth={auth} setAuth={setAuth} />}
           ></Route>
           {/* 대체 어떻게 props를 내릴 수 있단말인가 */}
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/welcome" element={<Welcome />}></Route>
-          <Route path="/introduce" element={<Introduce />}></Route>
-          <Route path="/ready" element={<Ready />}></Route>
-          <Route path="/board/*" element={<Board />}></Route>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/welcome' element={<Welcome />}></Route>
+          <Route path='/introduce' element={<Introduce />}></Route>
+          <Route path='/ready' element={<Ready />}></Route>
+          <Route path='/board/*' element={<Board />}></Route>
           <Route
-            path="/login"
+            path='/login'
             element={
               <Loginok setAuth={setAuth} setIsOpen={setIsOpen} auth={auth} />
             }
           ></Route>
           <Route
-            path="/login/idpw"
+            path='/login/idpw'
             element={<FindID />}
             setIsOpen={setIsOpen}
           />
@@ -83,10 +83,10 @@ function App() {
       </BrowserRouter>
       {/* <button>top</button> */}
       <button
-        className={BtnStatus ? "topBtn active" : "topBtn"} // 버튼 노출 여부
+        className={BtnStatus ? 'topBtn active' : 'topBtn'} // 버튼 노출 여부
         onClick={handleTop}
       >
-        <IoIosArrowDropupCircle size="50px" color="#6f76c1" />
+        <IoIosArrowDropupCircle size='50px' color='#6f76c1' />
       </button>
     </div>
   );
