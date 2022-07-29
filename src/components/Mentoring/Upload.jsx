41b97cb85452d40fbb5ed5b2898aa1d1107669cb
joyeajin <dpwls31200@gmail.com>
2022-07-29 17:44:40 +0900

    memtoriong, chemi 수정

--- src/components/Mentoring/Upload.jsx
+++ src/components/Mentoring/Upload.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "./css/index.module.css"
-import Diary from '../Diary/Diary';
+import styles from "./css/index.module.css";
+import Diary from "../Diary/Diary";
 import { BsCheckLg } from "react-icons/bs";
 
 const Upload = () => {
@@ -10,7 +10,9 @@ const Upload = () => {
         <BsCheckLg />
       </p>
       <h1 className={styles.title}>활동내역 업로드</h1>
-      <h3 className={styles.subTitle}>성찰저널 설명 글 성찰저널 설명 글 성찰저널 설명 글 성찰저널 설명 글 성찰저널 설명 글<br></br>성찰저널 설명 글 성찰저널 설명 글 성찰저널 설명 글 성찰저널 설명 글</h3>
+      <h3 className={styles.subTitle}>
+        멘토링 활동내역을 업로드해주세요. 멘토링 활동내역을 업로드해주세요.
+      </h3>
       <Diary />
     </div>
   );
